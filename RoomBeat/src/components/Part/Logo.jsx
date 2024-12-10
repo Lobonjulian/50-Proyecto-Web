c83ab6a49d6f40301d604b1b74a17fa2c56454db
Julian Lobon <julitolos6@gmail.com>
2024-12-10 09:48:26 -0500

    Estiliza el logotipo y mejora la capacidad de respuesta.
    Hace que se pueda hacer clic en el logotipo y enlaza a la página de inicio.
    Mejora la representación de imágenes y agrega un estilo responsivo para pantallas más pequeñas.

--- RoomBeat/src/components/Part/Logo.jsx
+++ RoomBeat/src/components/Part/Logo.jsx
@@ -4,10 +4,10 @@ import '../../styles/part.css'
 
 const Logo = () => {
   return (
-    <div className="logo">
+    <a  href="/" className="logo">
       <img src={LogoRoomBeat} alt="Logo de RoomBeats" />
       <p className="logo__text">RoomBeat</p>
-    </div>
+    </a>
   );
 };
 
